2cd3a54f8c57243944ad3236b4070827e95560f2
Chujo58 <92330130+Chujo58@users.noreply.github.com>
2025-01-10 20:58:33 -0500

    changes to dash only no more run

--- test_character.gd
+++ test_character.gd
@@ -1,35 +1,40 @@
 extends CharacterBody2D
 
+# Running/moving constants
 const DEFAULT_SPEED = 300.0
 const RUN_SPEED = 450.0
-const DASH_SPEED = 1800.0
+const DASH_SPEED = 10000.0
 @onready var SPEED = DEFAULT_SPEED
 
+# Dash with double tapping the 'R' key
 const DOUBLETAP_DELAY = .25
+const DASH_CD = 5
 const DASH_DELAY = 1
-var doubletap_time = DOUBLETAP_DELAY
-var dash_time = DASH_DELAY
-var last_keycode = 0
+@onready var doubletap_time = DOUBLETAP_DELAY
+@onready var dash_time = DASH_DELAY
+@onready var dash_timer_cd = Timer.new()
+@onready var last_keycode = 0
 
 func get_input():
 	var input_direction = Input.get_vector("Left","Right","Up","Down")
 	velocity = input_direction*SPEED
 
 func _input(event):
-	if event is InputEventKey and event.is_pressed():
-		if last_keycode == event.keycode and doubletap_time > 0 and dash_time > 0:
+	if event is InputEventKey:
+		if last_keycode == event.keycode and doubletap_time > 0 and dash_time > 0 and dash_timer_cd.time_left <= 0:
 			if String.chr(last_keycode) == 'R':
 				SPEED = DASH_SPEED
 			last_keycode = 0
 		else:
 			last_keycode = event.keycode
 			if dash_time < 0:
-				SPEED = RUN_SPEED
+				SPEED = DEFAULT_SPEED
 			
 		doubletap_time = DOUBLETAP_DELAY
+		dash_timer_cd.start(DASH_CD)
 		
-	if event.is_action_released("Run"):
-		SPEED = DEFAULT_SPEED
+	#if event.is_action_released("Run"):
+		#SPEED = DEFAULT_SPEED
 
 
 
@@ -39,6 +44,3 @@ func _physics_process(delta: float) -> void:
 
 	doubletap_time -= delta
 	dash_time -= delta
-
-	if Input.is_action_pressed("Run"):
-		SPEED = RUN_SPEED
